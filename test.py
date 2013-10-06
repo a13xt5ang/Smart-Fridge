fc6250c6280a8100aeff6c7030182a799b9b2ec4
a13xt5ang <me@a13xt5ang.com>
2013-10-05 22:43:57 -0400

    add test cases

---
+++ test.py
@@ -0,0 +1,31 @@
+from SimpleCV import Image, Color
+
+class Food:
+  APPLE = 1
+  BANANA = 2
+  REDBULL = 3
+
+def thirds(image):
+  first = image.crop(empty.width*0/3, 0, image.width/3, image.height)
+  second = image.crop(empty.width*1/3, 0, image.width/3, image.height)
+  third = image.crop(empty.width*2/3, 0, image.width/3, image.height)
+  return first, second, third
+
+empty = Image("empty.jpg")
+full = Image("appleredbull.jpg")
+e1, e2, e3 = thirds(empty)
+b1, b2, b3 = thirds(full)
+
+(b2).show()
+raw_input()
+(e2).show()
+raw_input()
+(e2-b2).show()
+raw_input()
+
+assert(scan_image("empty.jpg") == [])
+assert(scan_image("banana.jpg") == [Food.BANANA])
+assert(scan_image("appleredbull.jpg") == [Food.APPLE, Food.REDBULL])
+assert(scan_image("redbull.jpg") == [Food.REDBULL])
+assert(scan_image("redbullpepsi.jpg") == [Food.REDBULL, Food.PEPSI])
+assert(scan_image("redbullpepsiapple.jpg") == [Food.REDBULL, Food.PEPSI, Food.APPLE])
