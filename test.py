2dae25249c0ada1093ee4eade5d400275f9b8107
a13xt5ang <me@a13xt5ang.com>
2013-10-06 00:48:33 -0400

    template for detecting apples

--- test.py
+++ test.py
@@ -6,26 +6,64 @@ class Food:
   REDBULL = 3
 
 def thirds(image):
-  first = image.crop(empty.width*0/3, 0, image.width/3, image.height)
-  second = image.crop(empty.width*1/3, 0, image.width/3, image.height)
-  third = image.crop(empty.width*2/3, 0, image.width/3, image.height)
+  first = image.crop(image.width*0/3, 0, image.width/3, image.height)
+  second = image.crop(image.width*1/3, 0, image.width/3, image.height)
+  third = image.crop(image.width*2/3, 0, image.width/3, image.height)
   return first, second, third
 
-empty = Image("empty.jpg")
-full = Image("appleredbull.jpg")
-e1, e2, e3 = thirds(empty)
-b1, b2, b3 = thirds(full)
+def extract(empty, full):
+  mask = (full-empty).hueDistance(color=Color.BLACK).binarize()
+  return full.crop((full-mask.invert()).getPIL().getbbox())
 
-(b2).show()
-raw_input()
-(e2).show()
-raw_input()
-(e2-b2).show()
-raw_input()
+def fruit_color(empty, full):
+  extracted = extract(empty, full)
+  if extracted:
+    return extracted.meanColor()
+  return False
 
-assert(scan_image("empty.jpg") == [])
-assert(scan_image("banana.jpg") == [Food.BANANA])
-assert(scan_image("appleredbull.jpg") == [Food.APPLE, Food.REDBULL])
-assert(scan_image("redbull.jpg") == [Food.REDBULL])
-assert(scan_image("redbullpepsi.jpg") == [Food.REDBULL, Food.PEPSI])
-assert(scan_image("redbullpepsiapple.jpg") == [Food.REDBULL, Food.PEPSI, Food.APPLE])
+def pct_diff(a, b):
+  return (a-b)/((a+b)*2)*100
+
+def is_apple(color):
+  if color:
+    b, g, r = color
+    print color
+    print pct_diff(b, g)
+    print pct_diff((b+g)/1.2, r)
+    print ""
+  else:
+    print "no color"
+  return True
+
+def scan_image(empty, full):
+  empty = Image(empty)
+  full = Image(full)
+
+  e1, e2, e3 = thirds(empty)
+  b1, b2, b3 = thirds(full)
+
+  colors = [fruit_color(e1, b1),
+            fruit_color(e2, b2),
+            fruit_color(e3, b3)]
+
+  fruit = []
+
+  for color in colors:
+    if is_apple(color):
+      fruit.append(Food.APPLE)
+
+  return fruit
+
+assert(Food.APPLE not in scan_image("empty.jpg", "empty.jpg"))
+assert(Food.APPLE not in scan_image("empty.jpg", "banana.jpg"))
+assert(Food.APPLE in scan_image("empty.jpg", "appleredbull.jpg"))
+assert(Food.APPLE not in scan_image("empty.jpg", "redbull.jpg"))
+assert(Food.APPLE not in scan_image("empty.jpg", "redbullpepsi.jpg"))
+assert(Food.APPLE in scan_image("empty.jpg", "redbullpepsiapple.jpg"))
+
+assert(scan_image("empty.jpg", "empty.jpg") == [])
+assert(scan_image("empty.jpg", "banana.jpg") == [Food.BANANA])
+assert(scan_image("empty.jpg", "appleredbull.jpg") == [Food.APPLE, Food.REDBULL])
+assert(scan_image("empty.jpg", "redbull.jpg") == [Food.REDBULL])
+assert(scan_image("empty.jpg", "redbullpepsi.jpg") == [Food.REDBULL, Food.PEPSI])
+assert(scan_image("empty.jpg", "redbullpepsiapple.jpg") == [Food.REDBULL, Food.PEPSI, Food.APPLE])
